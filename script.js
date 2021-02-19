a6226d735a7874d4497855a279a8282918a32a62
Helder Silveira <helder.silveira.dev@gmail.com>
2021-02-19 16:16:23 -0300

    Coloquei ações nos botões.

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+
+function clicou(n) {
+    alert("Clicou em" +n);
+}
+
+function branco() {
+    alert("Clicou em BRANCO!");
+
+}
+
+function corrige() {
+    alert("Clicou em CORRIGE!");
+
+}
+
+function confirma() {
+    alert("Clicou em CONFIRMA!");
+
+}
