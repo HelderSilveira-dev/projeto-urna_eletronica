3be9b0dac1c4b200f35e1179348a4a0367b6b7e4
Helder Silveira <helder.silveira.dev@gmail.com>
2021-02-19 16:03:23 -0300

    Criado as variáveis do arquivo script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
