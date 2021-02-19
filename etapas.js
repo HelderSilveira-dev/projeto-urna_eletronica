261e0461a84081461d48360d671143028dbafeec
Helder Silveira <helder.silveira.dev@gmail.com>
2021-02-18 21:09:30 -0300

    atualização no candidados para vereador

---
+++ etapas.js
@@ -0,0 +1,27 @@
+let etapas=[
+{
+    titulo: "VEREADOR",
+    numero: 5,
+    candidatos: [
+        {
+            numero:38111,
+            nome: "Zé do Bode",
+            partido:POC,
+            fotos:[
+                {url:"", legenda:"Vereador", Smal: false}
+            ]
+
+        },
+        {
+            numero:77222,
+            nome: "Chico Penha",
+            partido:AMEBA,
+            fotos:[
+                {url:"", legenda:"Vereador", }
+            ]  
+        }
+    ]
+}
+]
+    
+    
